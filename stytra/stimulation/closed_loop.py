b9f51d272bb9e85ef777a1381a8c7178e0aaeecc
Vilim <vilim@neuro.mpg.de>
2017-10-03 15:54:20 +0200

    Initial version of position estimation with LSTM from tail

--- stytra/stimulation/closed_loop.py
+++ stytra/stimulation/closed_loop.py
@@ -1,7 +1,8 @@
 import numpy as np
 from stytra.tracking import QueueDataAccumulator
-# from keras.models import load_model
-
+from keras.models import load_model
+from bouter.kinematic_features import velocities_to_coordinates
+import datetime
 
 class VigourMotionEstimator:
     def __init__(self, data_acc, vigour_window=0.050):
@@ -20,12 +21,22 @@ class VigourMotionEstimator:
         return np.std(past_tail_motion[:, 1])
 
 
-class LSTMMotionEstimator:
-    def __init__(self, data_acc, LSTM_file, PCA_weights, gains=[1,1,1], delay=0):
+class LSTMLocationEstimator:
+    def __init__(self, data_acc, LSTM_file, PCA_weights,
+                 gains=[1,1,1], update_interval=100, lstm_sample_rate=500):
         assert (isinstance(data_acc, QueueDataAccumulator))
         self.data_acc = data_acc
         self.PCA_weights = PCA_weights
         self.model = load_model(LSTM_file)
+        self.n_past = self.model.input_shape[1]
+        self.delta_t = datetime.timedelta(seconds=self.n_past/lstm_sample_rate)
+        self.past_t = None
+        self.update_interval = update_interval
+        self.gains = gains
+        self.last_update = 0 # the position at which the last update occured
+        self.past_coordinates = np.zeros((update_interval, 3))
+        self.lstm_sample_rate = lstm_sample_rate
+        self.past_coords = None
 
     def get_displacement(self, delta_t_last):
         """ Calculates the position and rotation displacement using the LSTM
@@ -35,8 +46,28 @@ class LSTMMotionEstimator:
         :param delta_t_last:
         :return:
         """
-        n_past = self.model.input_shape[1]
-        tail = self.data_acc.get_last_n(n_past)[:, 1]
 
+        tail = self.data_acc.get_last_n(self.n_past)[:, 1]
         Y = self.model.predict(tail @ self.PCA_weights)
-        return Y
+
+        t_estimation = datetime.now()
+        if self.past_t is not None:
+
+            t_take_coords = t_estimation - self.delta_t
+            t_from_past_end  = (self.past_t-t_take_coords).total_seconds()
+            indexes_from_past_end = t_from_past_end*self.lstm_sample_rate
+
+            start_coords = self.past_coords[-indexes_from_past_end, :]
+
+        else:
+            start_coords = np.zeros(3)
+
+        self.past_t = t_estimation
+
+        new_coordinates = start_coords + \
+                          velocities_to_coordinates(Y,
+                                                    start_angle=start_coords[2])
+        self.past_coords = new_coordinates
+
+        return new_coordinates[-1]
+
