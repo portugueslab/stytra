7129863061bcb3f0fceabd521b5e582420645c16
Luigi Petrucco <luigi.petrucco@gmail.com>
2018-06-25 14:38:32 +0200

    Eyes+tail tracking implemented, refactored camera display for tracking to include both tail segment and eyes window

--- stytra/tracking/interfaces.py
+++ stytra/tracking/interfaces.py
@@ -2,7 +2,9 @@ from stytra.utilities import HasPyQtGraphParams
 
 
 class FrameProcessingMethod(HasPyQtGraphParams):
-    """The class for parametrisation of various tail and fish tracking methods"""
+    """The class for parametrisation of various tail and fish tracking
+    methods.
+    """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         for child in self.params.children():
@@ -22,21 +24,42 @@ class FrameProcessingMethod(HasPyQtGraphParams):
 Tail tracking methods 
 """
 
+class EmbeddedTrackingMethod(HasPyQtGraphParams):
+    """The class for parametrisation of various tail and fish tracking
+    methods.
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for child in self.params.children():
+            self.params.removeChild(child)
 
-class TailTrackingMethod(FrameProcessingMethod):
-    """General tail tracking method."""
-    def __init__(self):
-        super().__init__(name='tracking_tail_params')
-        # TODO maybe getting default values here:
-        standard_params_dict = dict(function=dict(values=['centroid',
-                                                          'angle_sweep'],
+        standard_params_dict = dict(image_scale=1.0,
+                                    filter_size=0,
+                                    color_invert=True,
+                                    function=dict(values=['centroid',
+                                                          'angle_sweep',
+                                                          'eye_threshold',
+                                                          'tail_eyes'],
                                                   value='centroid',
                                                   type='list',
                                                   readonly=False),
-                                    n_segments=dict(value=10,
+                                    )
+
+        for key in standard_params_dict.keys():
+            self.add_one_param(key, standard_params_dict[key])
+
+        self.accumulator_headers = None
+        self.data_log_name = None
+
+
+class TailTrackingMethod(EmbeddedTrackingMethod):
+    """General tail tracking method."""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # TODO maybe getting default values here:
+        standard_params_dict = dict(n_segments=dict(value=10,
                                                     type='int',
-                                                    limits=(1, 50)),
-                                    color_invert=True,
+                                                    limits=(2, 50)),
                                     tail_start=dict(value=(440, 225),
                                                     visible=False),
                                     tail_length=dict(value=(-250, 30),
@@ -54,7 +77,7 @@ class TailTrackingMethod(FrameProcessingMethod):
 class CentroidTrackingMethod(TailTrackingMethod):
     """Center-of-mass method to find consecutive segments."""
     def __init__(self):
-        super().__init__()
+        super().__init__(name='tracking_tail_params')
         self.params.child('function').setValue('centroid')
         standard_params_dict = dict(window_size=dict(value=30,
                                                      suffix=' pxs',
@@ -68,13 +91,9 @@ class CentroidTrackingMethod(TailTrackingMethod):
 class AnglesTrackingMethod(TailTrackingMethod):
     """Angular sweep method to find consecutive segments."""
     def __init__(self):
-        super().__init__()
+        super().__init__(name='tracking_tail_params')
         self.params.child('function').setValue('angle_sweep')
 
-
-""" 
-Eyes tracking methods 
-"""
 
 
 class FishTrackingMethod(FrameProcessingMethod):
@@ -87,20 +106,20 @@ class FishTrackingMethod(FrameProcessingMethod):
         self.data_log_name = ""
 
 
-class EyeTrackingMethod(FrameProcessingMethod):
+""" 
+Eyes tracking methods 
+"""
+
+
+class EyeTrackingMethod(EmbeddedTrackingMethod):
     """General eyes tracking method."""
-    def __init__(self):
-        super().__init__(name='tracking_tail_params')
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         # TODO maybe getting default values here:
-        self.add_params(function={'values': ['eye_threshold'],
-                                              'value': 'eye_threshold',
-                                              'type': 'list',
-                                              'readonly': True},
-                        color_invert=True,
-                        wnd_pos={'value': (140, 200),
-                                             'visible': False},
-                                    wnd_dim={'value': (110, 60),
-                                             'visible': False})
+        self.add_params(wnd_pos={'value': (140, 200),
+                                 'visible': False},
+                        wnd_dim={'value': (110, 60),
+                                 'visible': False})
 
         headers = []
         [headers.extend(['pos_x_e{}'.format(i), 'pos_y_e{}'.format(i),
@@ -112,19 +131,43 @@ class EyeTrackingMethod(FrameProcessingMethod):
 
 class ThresholdEyeTrackingMethod(EyeTrackingMethod):
     """Simple threshold method for finding eyes."""
-    def __init__(self):
-        super().__init__(name='tracking_fish_params')
+    def __init__(self, **kwargs):
+        super().__init__(name='tracking_eyes_params')
         standard_params_dict = dict(threshold=dict(value=64,
                                                    type='int',
                                                    limits=(0, 255)))
+
+        self.params.child('function').setValue('eye_threshold')
 
         for key, value in standard_params_dict.items():
             self.add_one_param(key, value)
 
 
-class TailEyesTrackingMethod(CentroidTrackingMethod,
-                             ThresholdEyeTrackingMethod):
-    pass
+class TailEyesTrackingMethod(TailTrackingMethod,
+                             EyeTrackingMethod):
+
+    def __init__(self):
+        super().__init__(name='tracking_eyes_tail_params')
+        standard_params_dict = dict(threshold=dict(value=64,
+                                                   type='int',
+                                                   limits=(0, 255))
+                                    )
+
+        self.params.child('function').setValue('tail_eyes')
+
+        for key, value in standard_params_dict.items():
+            self.add_one_param(key, value)
+
+        headers = ['tail_sum'] + \
+                   ['theta_{:02}'.format(i)
+                    for i in
+                    range(self.params['n_segments'])]
+        [headers.extend(['pos_x_e{}'.format(i), 'pos_y_e{}'.format(i),
+                         'dim_x_e{}'.format(i), 'dim_y_e{}'.format(i),
+                         'th_e{}'.format(i)]) for i in range(2)]
+
+        self.accumulator_headers = headers
+        self.data_log_name = 'behaviour_tail_eyes_log'
 
 
 class MovementDetectionParameters(HasPyQtGraphParams):
@@ -145,3 +188,7 @@ class MovementDetectionParameters(HasPyQtGraphParams):
         for key in standard_params_dict.keys():
             self.add_one_param(key, standard_params_dict[key])
 
+
+if __name__=='__main':
+    test = ThresholdEyeTrackingMethod()
+    print(test.get_clean_values())
