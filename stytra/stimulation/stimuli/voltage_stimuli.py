6ce6e0470768e68e67bc56a5222c9092e112e240
Unknown <virginiapalieri@gmail.com>
2018-10-16 12:07:47 +0200

    implemented LabJack voltage control

--- stytra/stimulation/stimuli/voltage_stimuli.py
+++ stytra/stimulation/stimuli/voltage_stimuli.py
@@ -1,12 +1,14 @@
-try:
-    import nidaqmx
-except:
-    print("No nidamax module found")
+#try:
+    #import nidaqmx
+#except:
+    #print("No nidamax module found")
 from stytra.stimulation.stimuli import Stimulus, InterpolatedStimulus
+from time import sleep
 
 
 class NIVoltageStimulus(Stimulus):
     def __init__(self, *args, dev="Dev1", chan="ao0"):
+        import nidaqmx
         self.dev = dev
         self.chan = chan
 
@@ -33,6 +35,31 @@ class InterpolatedVoltageStimulus(NIVoltageStimulus, InterpolatedStimulus):
             task.ao_channels.add_ao_voltage_chan("{}/{}".format(self.dev, self.chan))
             task.write(self.voltage)
 
-        # with nidaqmx.Task() as task:
-        #     task.ai_channels.add_ai_voltage_chan("Dev1/ai1")
-        #     print(task.read())
+        with nidaqmx.Task() as task:
+            task.ai_channels.add_ai_voltage_chan("Dev1/ai1")
+            print(task.read())
+
+
+
+class U3LabJackVoltageStimulus(Stimulus):
+    def __init__(self):
+        import u3
+        self.device = u3.U3()
+        self.chan = u3.DAC0_8
+
+class SetU3LabJackVoltageStimulus(U3LabJackVoltageStimulus):
+    def __init__(self, *args, voltage=0.0, **kwargs):
+        self.voltage = voltage
+        super().__init__(*args, **kwargs)
+
+    def start (self):
+        chan_value = self.device.voltageToDACBits(self.voltage, dacNumber=0, is16Bits=False)
+        self.device.getFeedback(self.chan(chan_value))
+
+if __name__ == "__main__":
+    stim = SetU3LabJackVoltageStimulus()
+    #for v in range (5):
+        #stim.voltage == v
+    stim.start()
+        #sleep(1)
+    print("sending pulse")
