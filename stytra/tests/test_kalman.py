1c26d71a8a9f11f30d86d7686912ea9f95b0b120
vilim <vilim.stih@gmail.com>
2018-10-17 19:58:28 +0200

    Completed Kalman filter test

--- stytra/tests/test_kalman.py
+++ stytra/tests/test_kalman.py
@@ -1,4 +1,13 @@
+import numpy as np
 from stytra.tracking.simple_kalman import NewtonianKalman
 
+
 def test_simple_kalman():
-    assert True
+    """ Tests the NewtonianKalman filter, compared to the reference
+    implementation in filterpy
+
+    """
+    kn = NewtonianKalman(0, 1.0, 0.02, 0.1)
+    kn.predict()
+    kn.update(1.0)
+    np.testing.assert_allclose(kn.x, np.array([0.66666667, 0.33333347]))
