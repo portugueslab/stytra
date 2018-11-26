d22681fb3dbcfde2b20f9d060a1acba59c1f286b
Luigi Petrucco <luigi.petrucco@gmail.com>
2018-11-26 14:21:02 +0100

    Added movie for running the examples. Display camera working with new movie

--- stytra/examples/display_camera_exp.py
+++ stytra/examples/display_camera_exp.py
@@ -1,17 +1,19 @@
 from stytra import Stytra
 from stytra.stimulation.stimuli import Pause
-
+from pathlib import Path
 from stytra.stimulation import Protocol
-import pkg_resources
-import tempfile
 
 
 class Nostim(Protocol):
     name = "empty protocol"
+    stytra_config = dict(camera=dict(
+        video_file=str(Path(__name__).parent / "assets" / "fish_compressed.h5")))
 
     def get_stim_sequence(self):
         return [Pause(duration=10)]
 
 
 if __name__ == "__main__":
+    pass
     s = Stytra(protocol=Nostim())
+#
