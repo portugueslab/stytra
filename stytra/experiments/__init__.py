eebd69144b85157dbaadcceac02c3f92e36e17ba
Unknown <luigi.petrucco@gmail.com>
2018-12-02 09:16:07 +0100

    Fixed bug with git when running script from outside stytra

--- stytra/experiments/__init__.py
+++ stytra/experiments/__init__.py
@@ -364,7 +364,8 @@ class Experiment(QObject):
                 self.dc.add_static_data(db_id, name="general/db_index")
 
                 # Get program name and version and save to the data_log:
-                repo = git.Repo(search_parent_directories=True)
+                repo = git.Repo(sys.argv[0],
+                                search_parent_directories=True)
                 git_hash = repo.head.object.hexsha
 
                 self.dc.add_static_data(dict(git_hash=git_hash,
