aad5d3c2736915af1c25c82e1ee4ce66c7be2b7d
Kata <kkoetter@neuro.mpg.de>
2019-10-17 11:46:48 +0200

    more comments
    more comments

--- stytra/stimulation/stimuli/relative_bg.py
+++ stytra/stimulation/stimuli/relative_bg.py
@@ -6,29 +6,31 @@ from stytra.stimulation.stimuli.closed_loop import FishTrackingStimulus
 
 #TODO detangle background x,y,theta from fish x,y, theta with constant remaining
 #TODO for that checking what they do in the mentioned stimuli above
+#TODO maybe program a GUI to show and rotate the stimulus in a controlled fashion
+#TODO get a fish video snippet where the fish behaves well for testing
 
 class  RelativeBackgroundStimulus(BackgroundStimulus):
+    """ This background shoudl be fully controllable on where it is placed.
+    It shoudl transform AND translate with the fish.
+    It should have a:
+     1) TRUE center = its own stimulus center (0,0)
+     2) RELATIVE center which can be set arbituary (e.g. to the fish) (fish x,y).
+     This relative center needs a kind of translation from fish coords
+     and maybe is also related to true center
+     3) Theta  which can be set """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         pass
 
     def get_rot_transform(self, w, h):
-        #Viims new defined fucntion. It could help in rewriting it
+        #Viims new defined fucntion for my stytra config code. It could help in rewriting it
         return (
         QTransform()
             .translate(-self.x, -self.y)
             .rotate(self.theta * 180 / np.pi)
             .translate(self.x, self.y)
         )
-
-
-
-
-
-
-
-
 
 
 
