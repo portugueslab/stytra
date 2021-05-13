35065f6c23671ecac7974334f6a87d19122a8461
Kata <katharina.koetter@tum.de>
2021-05-13 09:43:47 +0200

    bug fix tracking timeout

--- stytra/hardware/motor/motor_process.py
+++ stytra/hardware/motor/motor_process.py
@@ -88,16 +88,18 @@ class ReceiverProcess(Process):
 
             if self.tracking_event.is_set():
                 self.motor_status = status_type(*tracking_status)
+                time = datetime.datetime.now()
+
                 if last_position is not None:
                     self.motor_status = status_type(*tracking_status)
-                    time = datetime.datetime.now()
+                    #time = datetime.datetime.now()
                     pos_x = self.motor_x.get_position()
                     pos_y = self.motor_y.get_position()
 
                     # if it actually is tracking something
                     if abs(last_position.f0_x) > 0:
                         self.motor_status = status_type(*tracking_status)
-                        self.start_time = datetime.datetime.now()
+                        #self.start_time = datetime.datetime.now()
                         self.motor_x.move_rel(int(last_position.f0_x))
                         self.motor_y.move_rel(int(last_position.f0_y))
 
@@ -110,31 +112,25 @@ class ReceiverProcess(Process):
                             self.motor_status.waiting,
                         )
 
-                    # if tracking failure takes too long, go home and wait
-                    else:
-                        self.motor_status = status_type(*idle_status)
-                        e = (
-                            pos_x,
-                            pos_y,
-                            0.0,
-                            0.0,
-                            self.motor_status.tracking,
-                            self.motor_status.waiting,
-                        )
+                # if tracking failure takes too long, go home and wait
+                else:
+                    self.motor_status = status_type(*idle_status)
+                    self.start_time = datetime.datetime.now() #start counting idle time
+                    e = (pos_x, pos_y, 0.0, 0.0, self.motor_status.tracking,
+                            self.motor_status.waiting)
 
-                        if self.start_time is not None:
-                            #todo dont call if fish is tracked
-                            idle_time = (
-                                datetime.datetime.now() - self.start_time
+                    #if self.start_time is not None:
+
+                    idle_time = (datetime.datetime.now() - self.start_time
                             ).total_seconds()
-                            if idle_time > self.tracking_failure_timeout:
-                                print("tracking failure timeout called")
-                                self.motor_x.movesimple(position=self.home)
-                                self.motor_y.movesimple(position=self.home)
-                                self.start_time = None
+                    if idle_time > self.tracking_failure_timeout:
+                            print("tracking failure timeout called")
+                            self.motor_x.movesimple(position=self.home)
+                            self.motor_y.movesimple(position=self.home)
+                            self.start_time = None
 
-                    # save the output
-                    self.motor_position_queue.put(time, output_type(*e))
+                # save the output
+                self.motor_position_queue.put(time, output_type(*e))
 
         self.motor_x.close()
         self.motor_y.close()
