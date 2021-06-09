58ff550af8e67762b103cfb1edb32a8be28ee791
Ot Prat <43843799+OtPrat@users.noreply.github.com>
2021-06-09 10:25:32 +0200

    Small fic in NI voltage stimulus class

--- stytra/stimulation/stimuli/voltage_stimuli.py
+++ stytra/stimulation/stimuli/voltage_stimuli.py
@@ -16,10 +16,14 @@ except ImportError:
 
 
 class NIVoltageStimulus(Stimulus):
-    def __init__(self, *args, dev="Dev1", chan="ao0"):
+    def __init__(self, *args, dev="Dev1", chan="ao0", min_val=0, max_val=0, **kwargs):
 
         self.dev = dev
         self.chan = chan
+        self.min_val = min_val
+        self.max_val = max_val
+
+        super().__init__(*args, **kwargs)
 
 
 class SetVoltageStimulus(NIVoltageStimulus):
@@ -29,7 +33,8 @@ class SetVoltageStimulus(NIVoltageStimulus):
 
     def start(self):
         with nidaqmx.Task() as task:
-            task.ao_channels.add_ao_voltage_chan("{}/{}".format(self.dev, self.chan))
+            task.ao_channels.add_ao_voltage_chan("{}/{}".format(self.dev, self.chan),
+                                                 min_val=self.min_val, max_val=self.max_val5)
             task.write(self.voltage)
 
 
@@ -41,12 +46,14 @@ class InterpolatedVoltageStimulus(NIVoltageStimulus, InterpolatedStimulus):
     def update(self):
         super().update()
         with nidaqmx.Task() as task:
-            task.ao_channels.add_ao_voltage_chan("{}/{}".format(self.dev, self.chan))
+            task.ao_channels.add_ao_voltage_chan("{}/{}".format(self.dev, self.chan),
+                                                 min_val=self.min_val, max_val=self.max_val)
+
             task.write(self.voltage)
 
-        with nidaqmx.Task() as task:
-            task.ai_channels.add_ai_voltage_chan("Dev1/ai1")
-            print(task.read())
+        # with nidaqmx.Task() as task:
+        #     task.ai_channels.add_ai_voltage_chan("Dev1/ai1")
+        #     print(task.read())
 
 
 class U3LabJackVoltageStimulus(Stimulus):
