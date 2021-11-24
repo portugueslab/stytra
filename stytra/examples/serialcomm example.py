bdf2674c6942652742164d424ce19c22d054fcde
fedem-p <federico.puppo@tum.de>
2021-11-24 17:52:52 +0100

    added external hardware filter

--- stytra/examples/serialcomm example.py
+++ stytra/examples/serialcomm example.py
@@ -5,6 +5,7 @@ from stytra.hardware.serial import SerialConnection
 
 REQUIRES_EXTERNAL_HARDWARE = True
 
+
 class ArduinoCommStimulus(Stimulus):
     def __init__(self, com_port="COM3", baudrate=115200, **kwargs):
         super().__init__(**kwargs)
