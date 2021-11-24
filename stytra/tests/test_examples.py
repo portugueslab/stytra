90b232cf7530725193ebfc7e95671aefaed700e8
fedem-p <federico.puppo@tum.de>
2021-11-24 15:30:37 +0100

    deleted empty files; test all examples

--- stytra/tests/test_examples.py
+++ stytra/tests/test_examples.py
@@ -13,32 +13,29 @@ import stytra as st
 from pathlib import Path
 from pkgutil import iter_modules
 from importlib import import_module
-import pytest
 
 # iterate through the modules in the current package
 package_dir = Path(st.__file__).parent / "examples"
 
 protocols = []
 for (_, module_name, _) in iter_modules([package_dir]):
-    # Heuristic to exclude examples more complicated to run:
-    if all(
-        [
-            excl not in module_name
-            for excl in ["custom", "trigger", "serial", "arduino", "imaging_exp"]
-        ]
-    ):
-        # import the module and iterate through its attributes
-        try:
-            module = import_module(f"stytra.examples.{module_name}")
-        except ImportError as e:
-            print("Error in: {}\nSee full message here:\n{}".format(module_name, e))
 
+    # import the module and iterate through its attributes
+    try:
+        module = import_module(f"stytra.examples.{module_name}")
+    except ImportError as e:
+        print("Error in: {}\nSee full message here:\n{}".format(module_name, e))
+
+    #check if external harware is required to run the example
+    if 'REQUIRES_EXTERNAL_HARDWARE' in dir(module) and getattr(module, 'REQUIRES_EXTERNAL_HARDWARE') == True:
+            pass
+    else: 
         for attribute_name in dir(module):
-            if "Protocol" in attribute_name and attribute_name != "Protocol":
+             if "Protocol" in attribute_name and attribute_name != "Protocol":
                 protocols.append(getattr(module, attribute_name))
-            # attribute = getattr(module, attribute_name)
-        # except ModuleNotFoundError:
-        #    print(f"Can't import {module}")
+                # attribute = getattr(module, attribute_name)
+            # except ModuleNotFoundError:
+            #    print(f"Can't import {module}")
 
 
 @pytest.mark.parametrize("protocol", protocols)
